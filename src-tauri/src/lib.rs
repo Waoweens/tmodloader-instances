0c88d19ee6413f61cb21f56da3f606afa1d66628
Waoweens <30313089+Waoweens@users.noreply.github.com>
2026-01-20 16:46:52 +0700

    dialog test

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,16 +1,36 @@
+use tauri_plugin_dialog::DialogExt;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
 	format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn pick_tmod_dir(app: tauri::AppHandle) {
+	let ans = app
+		.dialog()
+		.message("File not found")
+		.kind(tauri_plugin_dialog::MessageDialogKind::Error)
+		.title("Warning")
+		.blocking_show();
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-	tauri::Builder::default()
+	let mut builder = tauri::Builder::default()
+		.plugin(tauri_plugin_fs::init())
 		.plugin(tauri_plugin_dialog::init())
 		.plugin(tauri_plugin_store::Builder::new().build())
 		.plugin(tauri_plugin_opener::init())
-		.invoke_handler(tauri::generate_handler![greet])
+		.invoke_handler(tauri::generate_handler![greet, pick_tmod_dir]);
+
+	#[cfg(debug_assertions)]
+	{
+		builder = builder.plugin(tauri_plugin_devtools::init());
+	}
+
+	builder
 		.run(tauri::generate_context!())
 		.expect("error while running tauri application");
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,27 @@
 <script lang="ts">
+	import { event } from "@tauri-apps/api";
 	import { invoke } from "@tauri-apps/api/core";
 
-	let name = $state("");
-	let greetMsg = $state("");
+	let tmodDir: string;
 
-	async function greet(event: Event) {
-		event.preventDefault();
-		// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-		greetMsg = await invoke("greet", { name });
+	async function save(e: SubmitEvent) {
+		e.preventDefault();
+	}
+
+	async function pickTmodDir() {
+		console.log("Picking tModLoader directory...");
+		await invoke("pick_tmod_dir");
 	}
 </script>
 
 <main class="container p-4">
 	<h1>tModLoader Instances</h1>
 
+	<form class="flex flex-col" onsubmit={save}>
+		<button class="mb-4 p-2 border rounded cursor-pointer" onclick={pickTmodDir}>Pick tModLoader directory</button>
+		<!-- <p>{tmodDir}</p> -->
 
+		<button type="submit">Save</button>
+	</form>
 </main>
 
-<style>
-
-</style>
